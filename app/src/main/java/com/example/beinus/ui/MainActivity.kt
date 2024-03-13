755b74cb4da2494ee9274c1c7cf147f7bc264915
world2222 <world2222@hanmail.net>
2024-03-13 21:28:04 +0900

    FEAT: Setup basic settings and bottom navigation
    Added all the essential dependencies for the project and basic bottom
    navigation. Edited some parts of the Manifest file.

---
+++ app/src/main/java/com/example/beinus/ui/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.beinus.ui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.example.beinus.graphs.RootNavGraph
+import com.example.beinus.ui.theme.BeinusTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            BeinusTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    RootNavGraph(navController = rememberNavController())
+                }
+            }
+        }
+    }
+}
